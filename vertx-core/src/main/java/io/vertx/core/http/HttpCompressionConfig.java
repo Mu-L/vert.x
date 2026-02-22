0dad3442538b32d396114f07dd9597b5dac8c78c
Julien Viet <julien@julienviet.com>
2026-02-22 21:15:30 +0100

    Improve HTTP compression config usability

--- vertx-core/src/main/java/io/vertx/core/http/HttpCompressionConfig.java
+++ vertx-core/src/main/java/io/vertx/core/http/HttpCompressionConfig.java
@@ -10,8 +10,14 @@
  */
 package io.vertx.core.http;
 
+import io.netty.handler.codec.compression.BrotliMode;
 import io.netty.handler.codec.compression.CompressionOptions;
+import io.netty.handler.codec.compression.DeflateOptions;
+import io.netty.handler.codec.compression.GzipOptions;
+import io.netty.handler.codec.compression.StandardCompressionOptions;
+import io.netty.handler.codec.compression.ZstdOptions;
 import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.core.impl.Arguments;
 
 import java.util.ArrayList;
@@ -107,13 +113,31 @@ public class HttpCompressionConfig {
   }
 
   /**
-   * Add a compressor.
+   * Set the list of compressors.
    *
-   * @see #setCompressors(List)
+   * @param compressors the list of compressors
+   * @return a reference to this, so the API can be used fluently
+   */
+  public HttpCompressionConfig setCompressors(List<CompressionOptions> compressors) {
+    this.compressors = compressors;
+    return this;
+  }
+
+  /**
+   * Add a compressor, if the compressor is already registered, the compressor is updated instead.
+   *
    * @return a reference to this, so the API can be used fluently
    */
   public HttpCompressionConfig addCompressor(CompressionOptions compressor) {
-    if (compressors == null) {
+    if (compressors != null) {
+      for (int i = 0;i < compressors.size();i++) {
+        CompressionOptions c = compressors.get(i);
+        if (c.getClass() == compressor.getClass()) {
+          compressors.set(i, compressor);
+          return this;
+        }
+      }
+    } else {
       compressors = new ArrayList<>();
     }
     compressors.add(compressor);
@@ -121,11 +145,145 @@ public class HttpCompressionConfig {
   }
 
   /**
-   * @param compressors the list of compressors
+   * Calls {@link #addCompressor(CompressionOptions)} with the gzip algorithm configured with default parameters.
+   *
    * @return a reference to this, so the API can be used fluently
    */
-  public HttpCompressionConfig setCompressors(List<CompressionOptions> compressors) {
-    this.compressors = compressors;
-    return this;
+  @GenIgnore
+  public HttpCompressionConfig addGzip() {
+    return addCompressor(StandardCompressionOptions.gzip());
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the gzip algorithm configured with the specified {@code compressionLevel}
+   * , default window bits and default mem level.
+   *
+   * @param compressionLevel the compression level, ranged from 1 to 9
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addGzip(int compressionLevel) {
+    GzipOptions def = StandardCompressionOptions.gzip();
+    return addCompressor(StandardCompressionOptions.gzip(compressionLevel, def.windowBits(), def.memLevel()));
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the gzip algorithm configured with the specified {@code compressionLevel}
+   * , specified {@code windowBits} and specified {@code memLevel}.
+   *
+   * @param compressionLevel the compression level param, ranged from 1 to 9
+   * @param windowBits the window bits param
+   * @param memLevel the mem level param
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addGzip(int compressionLevel, int windowBits, int memLevel) {
+    return addCompressor(StandardCompressionOptions.gzip(compressionLevel, windowBits, memLevel));
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the deflate algorithm configured with default parameters.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addDeflate() {
+    return addCompressor(StandardCompressionOptions.deflate());
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the deflate algorithm configured with the specified {@code compressionLevel}
+   * , default window bits and default mem level.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addDeflate(int compressionLevel) {
+    DeflateOptions def = StandardCompressionOptions.deflate();
+    return addCompressor(StandardCompressionOptions.deflate(compressionLevel, def.windowBits(), def.memLevel()));
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the deflate algorithm configured with the specified {@code compressionLevel}
+   * , specified {@code windowBits} and specified {@code memLevel}.
+   *
+   * @param compressionLevel the compression level param, ranged from 1 to 9
+   * @param windowBits the window bits param
+   * @param memLevel the mem level param
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addDeflate(int compressionLevel, int windowBits, int memLevel) {
+    return addCompressor(StandardCompressionOptions.deflate(compressionLevel, windowBits, memLevel));
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the snappy algorithm configured with default parameters.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addSnappy() {
+    return addCompressor(StandardCompressionOptions.snappy());
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the zstd algorithm configured with default parameters.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addZstd() {
+    return addCompressor(StandardCompressionOptions.zstd());
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the zstd algorithm configured with the specified {@code compressionLevel}
+   * , default block size and default max encode size.
+   *
+   * @param compressionLevel the compression level param
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addZstd(int compressionLevel) {
+    ZstdOptions def = StandardCompressionOptions.zstd();
+    return addCompressor(StandardCompressionOptions.zstd(compressionLevel, def.blockSize(), def.maxEncodeSize()));
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the zstd algorithm configured with the specified {@code compressionLevel}
+   * , specified {@code blockSize} and specified {@code maxEncodeSize}.
+   *
+   * @param compressionLevel the compression level param
+   * @param blockSize the block size param
+   * @param maxEncodeSize the max encode size param
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addZstd(int compressionLevel, int blockSize, int maxEncodeSize) {
+    ZstdOptions def = StandardCompressionOptions.zstd();
+    return addCompressor(StandardCompressionOptions.zstd(compressionLevel, blockSize, maxEncodeSize));
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the brotli algorithm configured with default parameters.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addBrotli() {
+    return addCompressor(StandardCompressionOptions.brotli());
+  }
+
+  /**
+   * Calls {@link #addCompressor(CompressionOptions)} with the brotli algorithm configured with the specified {@code quality}
+   * , default window and default mode.
+   *
+   * @param quality the quality param
+   * @return a reference to this, so the API can be used fluently
+   */
+  @GenIgnore
+  public HttpCompressionConfig addBrotli(int quality) {
+    return addCompressor(StandardCompressionOptions.brotli(quality, 4, BrotliMode.TEXT));
   }
 }
